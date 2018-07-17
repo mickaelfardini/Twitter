3994af25abcd0bcb394b91fb65dc0ef31450e654
Mickael Fardini <mickael.fardini@epitech.eu>
2018-07-17 14:39:36 +0200

    Controller et htaccess

--- conf/config.php
+++ conf/config.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../core/PDOConnect.php';
+require_once '../core/PDOConnection.php';
 
-$array['dbname'] = "tweeter";
+$array['dbname'] = "common-database";
 $array['host'] = "127.0.0.1";
 $array['user'] = "root";
 $array['pwd'] = "";
