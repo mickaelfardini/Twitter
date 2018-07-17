926024c33a6b16cf7e754f98cf922b5614f42b4e
Mickael Fardini <mickael.fardini@epitech.eu>
2018-07-17 15:36:11 +0200

    routeur

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
-
 require_once '../conf/config.php';
 spl_autoload_register(function ($class_name) {
     require_once "../core/" . $class_name . '.php';
 });
 
 Session::startSessionAction();
-$controller = isset($_GET['page']) ? ucfirst($_GET['page']) . "Controller" : "DefaultController";
-$model = isset($_GET['page']) ? ucfirst($_GET['page']) . "Model" : "DefaultModel";
+$controller = isset($_GET['page']) ? ucfirst($_GET['page']) . "Controller" : "IndexController";
+$model = isset($_GET['page']) ? ucfirst($_GET['page']) . "Model" : "IndexModel";
 $action = isset($_GET['action']) ? $_GET['action'] . "Action" : "defaultAction";
 include "controllers/" . ucfirst($controller) . ".php";
 include "models/" . ucfirst($model) . ".php";
