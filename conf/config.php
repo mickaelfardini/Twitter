d56f0c1076e1b5dc03de4111ce5c413c3a2552e0
Kallista <kallista.rhoc@epitech.eu>
2018-07-20 16:41:23 +0200

    conf dir

---
+++ conf/config.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../core/PDOConnection.php';
+
+$array['dbname'] = "common-database";
+$array['host'] = "localhost";
+$array['user'] = "root";
+$array['pwd'] = "R00t1234";
+
+$pdo = new PDOConnection($array);
