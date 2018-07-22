51372a9ed32e894f2c3af80fe2445e3d79957927
mickael.fardini <mickael.fardini@epitech.eu>
2018-07-22 19:33:02 +0200

    tweet to tag -> liaison tweet et hashtag

--- public/models/TweetModel.php
+++ public/models/TweetModel.php
@@ -41,7 +41,6 @@ class TweetModel
 			echo json_encode(["error" => "Le tweet ne doit pas exceder 140 caracteres."]);
 			return 0;
 		}
-		self::insertTagsAction($_POST['content']);
 		$query = "INSERT INTO tweet (id_user, content_tweet, delete_tweet)
 		VALUES (:id_user, :content_tweet, :delete_tweet)";
 		$req = PDOConnection::prepareAction($query);
@@ -49,6 +48,7 @@ class TweetModel
 			':id_user' => $_SESSION['id_user'],
 			':content_tweet' => $_POST['content'],
 			':delete_tweet' => 0));
+		self::insertTagsAction($_POST['content']);
 		echo json_encode(["PostTweet" => "ok"]);
 		return 1;
 	}
@@ -77,6 +77,12 @@ class TweetModel
 
 	private static function insertTagsAction($content)
 	{
+		$query = "SELECT id_tweet FROM tweet WHERE id_user = ?
+					ORDER BY date_tweet DESC LIMIT 1";
+		$req = PDOConnection::prepareAction($query);
+		$req->execute([$_SESSION['id_user']]);
+		$id_tweet = $req->fetch(PDO::FETCH_ASSOC)['id_tweet'];
+
 		preg_match_all("/#([a-zA-Z]+)/", $content, $matches);
 		foreach ($matches[1] as $match) {
 			$query = "SELECT * FROM hashtag WHERE name_hashtag = ?";
@@ -87,6 +93,19 @@ class TweetModel
 				$req = PDOConnection::prepareAction($query);
 				$req->execute([$match]);
 			}
+			self::tweetToTagAction($match, $id_tweet);
 		}
+	}
+
+	private static function tweetToTagAction($match, $id_tweet)
+	{
+		$query = "SELECT id_hashtag FROM hashtag WHERE name_hashtag = ?";
+		$req = PDOConnection::prepareAction($query);
+		$req->execute([$match]);
+		$id_hashtag = $req->fetch(PDO::FETCH_ASSOC)['id_hashtag'];
+
+		$query = "INSERT INTO tweet_to_tag (id_tweet, id_tag) VALUES (?, ?)";
+		$req = PDOConnection::prepareAction($query);
+		$req->execute([$id_tweet, $id_hashtag]);
 	}
 }
