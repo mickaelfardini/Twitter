49da7cd8d29108c35e18238fb0ca17cc679091b1
mickael.fardini <mickael.fardini@epitech.eu>
2018-07-24 12:48:44 +0200

    message front vite fait

--- public/views/hashtags.php
+++ public/views/hashtags.php
@@ -2,7 +2,9 @@
 $countTweet = Controller::countTweetsAction()[0];
 $hashtags = Controller::countTagsAction();
 include 'inc/header.php';
-include 'inc/navbar.php'; ?>
+include 'inc/navbar.php';
+include 'inc/messages.php'; ?>
+
 	<div class="main">
 		<div class="side">
 			<div class="left-bar">
@@ -39,6 +41,9 @@ include 'inc/navbar.php'; ?>
 		</div>
 	</div>
 	<script src="/Twitter/public/js/jquery-3.3.1.js"></script>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> -->
+<script src="/Twitter/public/js/bootstrap.min.js"></script>
 	<script src="/Twitter/public/js/script.js"></script>
 	<script src="/Twitter/public/js/tweets.js"></script>
 </body>
