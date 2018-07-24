43c45d6e83362b09bfe822b2b75b43224532c645
Kallista <kallista.rhoc@epitech.eu>
2018-07-24 11:44:52 +0200

    conditions s'il n'y a pas de tweets + ajout de [register_date] pour le profil

--- public/views/profile.php
+++ public/views/profile.php
@@ -3,16 +3,14 @@ include 'inc/header.php';
 include 'inc/navbar.php';
 
 $user = ProfileController::getUserInfo();
-// Recup les infos du gars (user ou personne dans l'url apres le /profile/...)
-// Var Dump a retirer, juste histoire de montrer comment recup les infos et comment elles se presentent.
 ?>
 <div class=" main user">
 	<div class="side left-bar">
 		<img src="<?=$_SESSION["avatar"]?>" alt="icon" class="icon"><!-- Recup l'icon de l'user -->
 		<ul class="prof navbar-nav">
-			<li class="nav-item" id="firstname"><?=$user[0]["firstname"]?></li>
-			<li class="nav-item" id="username"><a class="nav-link" href="/Twitter/profile">@<?=$user[0]['username']?></a></li>
-			<li class="nav-item" id="register_date">Member since <?=substr($user[0]["register_date"], 0, -8)?></li>
+			<li class="nav-item" id="firstname"><?=$_SESSION["firstname"]?></li>
+			<li class="nav-item" id="username"><a class="nav-link" href="/Twitter/profile">@<?=$_SESSION['username']?></a></li>
+			<li class="nav-item" id="register_date">Member since <?=substr($_SESSION["register_date"], 0, -8)?></li>
 			<li class="nav-item" id="editProfile"><a href="#">Edit profile</a></li>
 		</ul>
 	</div>	
@@ -20,9 +18,13 @@ $user = ProfileController::getUserInfo();
 		<div>
 			<h4>Tweets:</h4>
 			<ol class="list-group">
-			<?php	foreach($user as $u){?>
-				<li class="list-group-item tweet"><a class="nav-link" href="/Twitter/profile">@<?=$_SESSION['username']?></a><br><?=($u["content_tweet"]);?></li>
-			<?php } ?>
+			<?php	if(empty($user))
+						echo "No tweets to show.";
+				
+					else{
+					foreach($user as $u){?>
+					<li class="list-group-item tweet"><a class="nav-link" href="/Twitter/profile">@<?=$_SESSION['username']?></a><br><?=($u["content_tweet"]);?></li>
+			<?php } }?>
 			</ol>
 		</div>	
 
