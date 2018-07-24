edb60ba7197622ea8c770eca95cc6b7e2e86f66b
mickael.fardini <mickael.fardini@epitech.eu>
2018-07-24 19:13:30 +0200

    footer script

--- public/views/profile.php
+++ public/views/profile.php
@@ -31,10 +31,6 @@ $tweets = ProfileController::getUserTweets();
 		</div>
 	</div>
 </div>
-<script src="/Twitter/public/js/jquery-3.3.1.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="/Twitter/public/js/bootstrap.min.js"></script>
-<script src="/Twitter/public/js/script.js"></script>
-<script src="/Twitter/public/js/tweets.js"></script>
+<?php include 'inc/footer.php';?>
 </body>
 </html>
