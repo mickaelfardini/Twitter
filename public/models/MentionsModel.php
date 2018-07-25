7f0ad6156d3c3d0f4312aed2a73f3b6027160901
mickaelfardini <38546038+mickaelfardini@users.noreply.github.com>
2018-07-25 15:54:18 +0200

    Update MentionsModel.php

--- public/models/MentionsModel.php
+++ public/models/MentionsModel.php
@@ -4,7 +4,10 @@ class MentionsModel
 {
 	public static function getMentions($username)
 	{
-		$query = "SELECT id_tweet, content_tweet, date_tweet, username, avatar FROM tweet 	JOIN user ON tweet.id_user = user.id_user WHERE content_tweet LIKE :username ORDER BY date_tweet DESC";
+		$query = "SELECT id_tweet, content_tweet, date_tweet, username, avatar 
+		FROM tweet
+		JOIN user ON tweet.id_user = user.id_user 
+		WHERE content_tweet LIKE :username ORDER BY date_tweet DESC";
 		$sql = PDOConnection::prepareAction($query);
 		$sql->bindValue(":username", "%@".$username."%");
 		$sql->execute();
