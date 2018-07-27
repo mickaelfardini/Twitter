a32cf0aad65c429c063d3490565f32a116e08fef
Kallista <kallista.rhoc@epitech.eu>
2018-07-27 11:09:17 +0200

    navbar respon ok + width navbar ok avec messages popup

--- public/inc/navbar.php
+++ public/inc/navbar.php
@@ -1,5 +1,5 @@
 <body style="background-color: <?=$_SESSION['theme']?>">
-	<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between" data-spy="affix" data-offset-top="197">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
