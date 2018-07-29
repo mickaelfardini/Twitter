c7330cada1280ad988940159218c8374a19f4ace
Chris <christophe.debese@epitech.eu>
2018-07-29 22:38:05 +0200

    le mdp c'est dur

--- public/models/AccountModel.php
+++ public/models/AccountModel.php
@@ -15,8 +15,10 @@ class AccountModel
 	{
 		$query = "UPDATE user SET ";
 		foreach ($_POST as $key => $value) {
-			if (!empty($value)) {
+			if (!empty($value) && $key != "password") {
 				$query .= "$key = '$value' , ";
+			} elseif (!empty($value) && $key == "password") {
+				self::ChangePassAction();
 			}
 		}
 		$query = rtrim($query,", ");
@@ -27,43 +29,43 @@ class AccountModel
 		if(isset($_POST['theme'])){
 			$_SESSION['theme'] = $_POST['theme'];
 		}
-		//header('Location: /Twitter/profile');
+		header('Location: /Twitter/profile');
 	}
 
 	public function checkPassAction()
-{
-     $query = "SELECT password FROM user WHERE id_user = ?";
-        $sql = PDOConnection::prepareAction($query);
-        $sql->execute([$_SESSION['id_user']]);
-        $db_password = $sql->fetch();
-        
-        $input_password = $_POST["password"];
-        $input_password .= "si tu aimes la wac tape dans tes mains";
-        $hashed_input = hash('ripemd160', $input_password);
-var_dump($db_password);
-        if($db_password != $hashed_input)
-            return false;
-        else
-            return true;
-}
+	{
+		$query = "SELECT password FROM user WHERE id_user = ?";
+		$sql = PDOConnection::prepareAction($query);
+		$sql->execute([$_SESSION['id_user']]);
+		$db_password = $sql->fetch();
+		
+		$input_password = $_POST["password"];
+		$input_password .= "si tu aimes la wac tape dans tes mains";
+		$hashed_input = hash('ripemd160', $input_password);
+		var_dump($db_password);
+		if($db_password != $hashed_input)
+			return false;
+		else
+			return true;
+	}
 
-public function ChangePassAction()
-{
-	var_dump(checkPassAction());
-   if (!checkPassAction()){
-        $alert = "Incorrect password combination";
-     echo json_encode(array("error"=>"$alert"));
-   }
-    
-    else {
-        $new_password = $_POST["newpassword"];
-        $new_password .= "si tu aimes la wac tape dans tes mains";
+	public function ChangePassAction()
+	{
+		var_dump(checkPassAction());
+		if (!checkPassAction()){
+			$alert = "Incorrect password combination";
+			echo json_encode(array("error"=>"$alert"));
+		}
+		
+		else {
+			$new_password = $_POST["newpassword"];
+			$new_password .= "si tu aimes la wac tape dans tes mains";
 
-        $hashed = hash('ripemd160', $new_password);
-var_dump($hashed);
-        $query = "UPDATE user SET password = ? where id_user = ?";
-        $sql = PDOConnection::prepareAction($query);
-        $sql->execute([$hashed, $_SESSION["id_user"]]);
-    }
-}
+			$hashed = hash('ripemd160', $new_password);
+			var_dump($hashed);
+			$query = "UPDATE user SET password = ? where id_user = ?";
+			$sql = PDOConnection::prepareAction($query);
+			$sql->execute([$hashed, $_SESSION["id_user"]]);
+		}
+	}
 }
